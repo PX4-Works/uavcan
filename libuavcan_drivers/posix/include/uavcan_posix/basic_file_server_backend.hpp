aa27af6392d560a82d2b1485d3bd4b4b398e637f
David Sidrane <david_s5@nscdg.com>
2015-05-29 14:45:14 -1000

    Wip file open cache

--- libuavcan_drivers/posix/include/uavcan_posix/basic_file_server_backend.hpp
+++ libuavcan_drivers/posix/include/uavcan_posix/basic_file_server_backend.hpp
@@ -34,7 +34,276 @@ class BasicFileSeverBackend : public uavcan::IFileServerBackend
 {
     enum { FilePermissions = 438 };   ///< 0o666
 
+
 protected:
+
+    class FDCacheBase {
+
+    public:
+        FDCacheBase() { }
+        virtual ~FDCacheBase() { }
+
+        virtual int open(const char *path, int oflags)
+        {
+            using namespace std;
+
+            return ::open(path, oflags);
+        }
+
+        virtual int close(int fd)
+        {
+            using namespace std;
+
+            return ::close(fd);
+        }
+
+    };
+
+    FDCacheBase fallback_;
+
+    class FDCache : public FDCacheBase {
+
+        class FDCacheItem {
+
+            friend FDCache;
+            FDCacheItem *next_;
+            int refcnt_;
+            int fd_;
+            int oflags_;
+            const char *path_;
+
+        public:
+
+            enum { InvalidFD = -1 };
+
+            FDCacheItem() :
+                next_(0),
+                refcnt_(0),
+                fd_(InvalidFD),
+                oflags_(0),
+                path_(0)
+            { }
+
+            FDCacheItem(int fd, const char * path, int oflags) :
+                next_(0),
+                refcnt_(0),
+                fd_(fd),
+                oflags_(oflags),
+                path_(strdup(path))
+            {
+
+            }
+
+            ~FDCacheItem()
+            {
+                if (valid())
+                {
+                    delete path_;
+                }
+            }
+
+            inline bool valid()
+            {
+                return path_ != 0;
+            }
+
+            inline int getFD()
+            {
+                return fd_;
+            }
+
+            inline int getRefCount()
+            {
+                return refcnt_;
+            }
+
+            int addRef()
+            {
+                refcnt_++;
+                return getRefCount();
+            }
+
+            int removeRef()
+            {
+                refcnt_--;
+                return getRefCount();
+            }
+
+            int compare(const char * path, int oflags)
+            {
+                return (oflags_ == oflags && 0 == ::strcmp(path, path_)) ? 0 : 1;
+            }
+
+            int compare(int fd)
+            {
+                return fd_ == fd ? 0 : 1;
+            }
+
+        };
+
+        FDCacheItem* head_;
+
+        FDCacheItem* find(const char *path, int oflags)
+        {
+            FDCacheItem* pi = head_;
+            while(pi && 0 != pi->compare(path, oflags))
+            {
+                pi = pi->next_;
+            }
+            return pi;
+        }
+
+        FDCacheItem* find(int fd)
+        {
+            FDCacheItem* pi = head_;
+            while(pi && 0 != pi->compare(fd))
+            {
+                pi = pi->next_;
+            }
+            return pi;
+        }
+
+
+        FDCacheItem* add(FDCacheItem* pi)
+        {
+            pi->next_ = head_;
+            head_ = pi;
+            pi->addRef();
+            return pi;
+        }
+
+        FDCacheItem* remove(FDCacheItem* pi)
+        {
+            if (pi->removeRef() > 0 )
+            {
+                pi = 0;
+            }
+            else
+            {
+
+                FDCacheItem** li = &head_;
+                FDCacheItem* i = head_;
+                while(i)
+                {
+                    if (i == pi)
+                    {
+                        *li = pi->next_;
+                        break;
+                    }
+                    li = &(i->next_);
+                    i = i->next_;
+                }
+            }
+            return pi;
+        }
+
+    public:
+
+        FDCache() :
+            head_(0)
+        { }
+
+        virtual ~FDCache()
+        {
+            for(FDCacheItem* pi = head_; pi; )
+            {
+                FDCacheItem* tmp = pi->next_;
+                (void)FDCacheBase::close(pi->fd_);
+                delete pi;
+                pi = tmp;
+
+            }
+        }
+
+        virtual int open(const char *path, int oflags)
+        {
+            int fd = FDCacheItem::InvalidFD;
+
+            FDCacheItem *pi = find(path, oflags);
+
+            if (pi != 0)
+            {
+                pi->addRef();
+            }
+            else
+            {
+                fd = FDCacheBase::open(path, oflags);
+                if (fd < 0)
+                {
+                    return fd;
+                }
+
+                /* Allocate and clone path */
+
+                pi = new FDCacheItem(fd, path, oflags);
+
+                /* Allocation worked but check clone */
+
+                if (pi && !pi->valid())
+                {
+
+                    /* Allocation worked but clone or path failed */
+
+                    delete pi;
+                    pi = 0;
+                }
+
+                if (pi == 0)
+                {
+                    /*
+                     * If allocation fails no harm just can not cache it
+                     * return open fd
+                     */
+
+                    return fd;
+                }
+                /* add new */
+                add(pi);
+            }
+            return pi->getFD();
+        }
+
+
+        virtual int close(int fd)
+        {
+            FDCacheItem *pi = find(fd);
+            if (pi == 0)
+            {
+                /*
+                 * If not found just close it
+                 */
+
+                return FDCacheBase::close(fd);
+            }
+            pi = remove(pi);
+            if (pi)
+            {
+                delete pi;
+                return FDCacheBase::close(fd);
+            }
+            return 0;
+        }
+
+    };
+
+
+    FDCacheBase *fdcache_;
+
+    FDCacheBase&  getFDCache()
+    {
+        if (fdcache_ == 0)
+        {
+            fdcache_ = new FDCache();
+
+            if (fdcache_ == 0)
+            {
+                fdcache_ = &fallback_;
+
+            }
+        }
+        return *fdcache_;
+    }
+
     /**
      * Back-end for uavcan.protocol.file.GetInfo.
      * Implementation of this method is required.
@@ -48,6 +317,7 @@ protected:
         if (path.size() > 0)
         {
             using namespace std;
+
             out_size = 0;
             out_crc64 = 0;
 
@@ -99,7 +369,7 @@ protected:
                 rv = 0;
             out_close:
                 PROBE_MARK(4);
-                close(fd);
+                (void)::close(fd);
                 PROBE_MARK(4);
             }
         }
@@ -114,9 +384,7 @@ protected:
      * if the end of file is reached.
      * On success the method must return zero.
      */
-
-    Path last_path;
-    int last_fd;
+    __attribute__((optimize("O0")))
     virtual int16_t read(const Path& path, const uint32_t offset, uint8_t* out_buffer, uint16_t& inout_size)
     {
         PROBE(5, true);
@@ -124,35 +392,10 @@ protected:
 
         if (path.size() > 0)
         {
-            int fd = -1;
-
-            if (last_fd != -1 &&
-                last_path.size() != 0 &&
-                0 == strcmp(path.c_str(), last_path.c_str()))
-            {
-
-                fd = last_fd;
-
-            }
-            else
-            {
-
-                if (last_fd != -1)
-                {
-                    (void)close(last_fd);
-                    last_fd = -1;
-                }
-
-                last_path = path;
-                PROBE(4, true);
-                fd = open(path.c_str(), O_RDONLY);
-                PROBE(4, false);
-                if (fd >= 0)
-                {
-                    last_fd = fd;
-                }
-                PROBE(4, false);
-            }
+            FDCacheBase& cache = getFDCache();
+            PROBE(4, true);
+            int fd = cache.open(path.c_str(), O_RDONLY);
+            PROBE(4, false);
 
             if (fd < 0)
             {
@@ -164,6 +407,8 @@ protected:
                 rv = ::lseek(fd, offset, SEEK_SET);
                 PROBE(4, false);
 
+                ssize_t len = 0;
+
                 if (rv < 0)
                 {
                     rv = errno;
@@ -172,7 +417,7 @@ protected:
                 {
                     // TODO use a read at offset to fill on EAGAIN
                     PROBE(4, true);
-                    ssize_t len = ::read(fd, out_buffer, inout_size);
+                    len = ::read(fd, out_buffer, inout_size);
                     PROBE(4, false);
 
                     if (len < 0)
@@ -181,19 +426,18 @@ protected:
                     }
                     else
                     {
-
-                        inout_size = len;
                         rv = 0;
                     }
                 }
-                if (rv == 0 && inout_size == 0)
+
+                if (rv != 0 || len != inout_size)
                 {
-                  PROBE(4, true);
-                  (void)close(fd);
-                  last_path.clear();
-                  last_fd = -1;
-                  PROBE(4, false);
+                    PROBE(4, true);
+                    (void)cache.close(fd);
+                    PROBE(4, false);
                 }
+
+                inout_size = len;
             }
         }
         PROBE(5, false);
@@ -202,9 +446,20 @@ protected:
     }
 
 public:
+
     BasicFileSeverBackend() :
-        last_fd(-1)
-    { }
+        fdcache_(0)
+    {
+    }
+
+    ~BasicFileSeverBackend()
+    {
+        if (fdcache_ != &fallback_)
+        {
+            delete fdcache_;
+            fdcache_ = 0;
+        }
+    }
 };
 
 }
